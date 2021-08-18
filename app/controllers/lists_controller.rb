4df0d079a748c1bc4a5984dcd6e4fdb0588fd902
Orlando Altamiranda <orlandoaltamiranda@hotmail.com>
2021-08-18 18:19:35 -0400

    Add Controller, Routes, view of list and bookmarks

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  def bookmarks
+    @list = List.find(params[:id])
+    @list.bookmarks
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
